593201769c3b28c298f5ff0cbe7bc51fd05daae3
abdularshath <abdularshath.a@gmail.com>
2025-03-20 15:38:30 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+class main{
+  public static void main(System[]args){
+    LinkedList <String> bike =new LinkedList<String>();
+    bike.add("Ns200");
+    bike.add("R15");
+    bike.add("CBR250");
+    bike.add("Duke200");
+    bike.addfirst("RC200");
+    bike.addLast("Apache310RR");
+    System.out.println(bikes);
+}
+}
